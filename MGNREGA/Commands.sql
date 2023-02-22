c169c79a3516d9a8733e89917fc71394778eb6f6
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 13:50:55 +0530

    update

---
+++ MGNREGA/Commands.sql
@@ -0,0 +1,10 @@
+-- Gram Panchayat Member (gpm) Table:
+
+ create table gpm( gid int primary key, gname varchar(25) not null, email_id varchar(50) unique not null, password varchar(12) not null );
+ 
+ -- Employee Table:
+ 
+ 
+ -- Project Table;
+ 
+ 
