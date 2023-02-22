c00a57203dc2a8d9743f7b6934beb9cb18561be9
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 21:11:33 +0530

    day-2

--- MGNREGA/src/com/dto/ProjectImp.java
+++ MGNREGA/src/com/dto/ProjectImp.java
@@ -16,6 +16,59 @@ public class ProjectImp {
 		this.eid = eid;
 		this.gid = gid;
 	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public double getPcost() {
+		return pcost;
+	}
+
+	public void setPcost(double pcost) {
+		this.pcost = pcost;
+	}
+
+	public int getPduration() {
+		return pduration;
+	}
+
+	public void setPduration(int pduration) {
+		this.pduration = pduration;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public int getGid() {
+		return gid;
+	}
+
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
+
+	@Override
+	public String toString() {
+		return "ProjectImp Id = " + pid + ", Name = " + pname + ", Cost = " + pcost + ", Duration = " + pduration;
+	}
 	
 	
 }
