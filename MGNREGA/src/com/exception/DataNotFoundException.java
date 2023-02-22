9a99e0c51d3942f2dc9d258740639bbbd30261a2
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 14:20:27 +0530

    day_2

---
+++ MGNREGA/src/com/exception/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+public class DataNotFoundException extends Exception{
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+	
+	@Override
+	public String toString() {
+		return toString();
+	}
+}
