3f1f17a4112e3c97678437fcac9eb9f843d4a168
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 23:36:07 +0530

    day-2

--- MGNREGA/src/com/dto/Project.java
+++ MGNREGA/src/com/dto/Project.java
@@ -1,5 +1,47 @@
 package com.dto;
 
 public interface Project {
+	public int getPid();
+	
+	public void setPid(int pid);
+	
 
+	public String getPname();
+	
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public double getPcost() {
+		return pcost;
+	}
+
+	public void setPcost(double pcost) {
+		this.pcost = pcost;
+	}
+
+	public int getPduration() {
+		return pduration;
+	}
+
+	public void setPduration(int pduration) {
+		this.pduration = pduration;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public int getGid() {
+		return gid;
+	}
+
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
 }
