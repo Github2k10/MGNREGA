9445487d6c5484a503595c5154c13d485f626a34
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 17:13:48 +0530

    day_2

--- MGNREGA/src/com/dto/ProjectImp.java
+++ MGNREGA/src/com/dto/ProjectImp.java
@@ -1,5 +1,21 @@
 package com.dto;
 
 public class ProjectImp {
-
+	private int pid;
+	private String pname;
+	private double pcost;
+	private int pduration;
+	private int eid;
+	private int gid;
+	
+	public ProjectImp(int pid, String pname, double pcost, int pduration, int eid, int gid) {
+		this.pid = pid;
+		this.pname = pname;
+		this.pcost = pcost;
+		this.pduration = pduration;
+		this.eid = eid;
+		this.gid = gid;
+	}
+	
+	
 }
