3d54f8403b21371d4bd04e43b9427096f36179d4
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-22 14:12:00 +0530

    update

--- MGNREGA/src/com/ui/Main.java
+++ MGNREGA/src/com/ui/Main.java
@@ -8,7 +8,22 @@ import com.dao.ConnectToDataBase;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Connection connection = null;
+		try {
+			connection = ConnectToDataBase.makeConnnection();
+			System.out.println("connection stablished");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				ConnectToDataBase.closeConnection(connection);
+				System.out.println("connection closed");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
