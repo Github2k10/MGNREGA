42edecf6ad79615e648d9a21224c4198c6a86ef5
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-23 13:01:38 +0530

    day-3

--- MGNREGA/src/com/dto/GPM.java
+++ MGNREGA/src/com/dto/GPM.java
@@ -1,5 +1,49 @@
 package com.dto;
 
 public interface GPM {
+	public int getEid();
 
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 }
