d5c1884d0d26b9f7ec0e89c323fd6a6f001c2651
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-23 12:26:14 +0530

    day-3

--- MGNREGA/src/com/dto/EmployeeImp.java
+++ MGNREGA/src/com/dto/EmployeeImp.java
@@ -1,5 +1,14 @@
 package com.dto;
 
-public class EmployeeImp {
+import java.time.LocalDate;
 
+public class EmployeeImp {
+	private int eid;
+	private String name;
+	private String email;
+	private int working_day;
+	private int pid;
+	private LocalDate dob;
+	private int gid;
+	
 }
