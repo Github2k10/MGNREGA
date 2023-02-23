deb685a7554860f74d242496de3385cbff1833cb
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-23 12:59:19 +0530

    day-3

--- MGNREGA/src/com/dto/GPMImp.java
+++ MGNREGA/src/com/dto/GPMImp.java
@@ -1,5 +1,12 @@
 package com.dto;
 
 public class GPMImp {
-
+	private int eid;
+	private String name;
+	private String email;
+	private String password;
+	private String district;
+	private String state;
+	
+	
 }
