738ae2200facd9850d76ec5594ffe7134ce2e664
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-23 12:29:32 +0530

    day-3

--- MGNREGA/src/com/dto/Employee.java
+++ MGNREGA/src/com/dto/Employee.java
@@ -1,5 +1,33 @@
 package com.dto;
 
-public interface Employee {
+import java.time.LocalDate;
 
+public interface Employee {
+	public int getEid();
+	
+	public void setEid(int eid);
+	
+	public String getName();
+	
+	public void setName(String name);
+	
+	public String getEmail();
+	
+	public void setEmail(String email);
+	
+	public int getWorking_day();
+	
+	public void setWorking_day(int working_day);
+	
+	public int getPid();
+	
+	public void setPid(int pid);
+	
+	public LocalDate getDob() ;
+	
+	public void setDob(LocalDate dob) ;
+	
+	public int getGid();
+	
+	public void setGid(int gid);
 }
