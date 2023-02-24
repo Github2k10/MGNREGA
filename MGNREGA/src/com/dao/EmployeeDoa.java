4d0e80132d50be8a5f1826ae860f3861def71087
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-24 13:50:00 +0530

    day_4

--- MGNREGA/src/com/dao/EmployeeDoa.java
+++ MGNREGA/src/com/dao/EmployeeDoa.java
@@ -1,5 +1,32 @@
 package com.dao;
 
-public class EmployeeDoa {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.dto.Employee;
+import com.exception.SomethingWentWrong;
+
+public class EmployeeDoa {
+	public static boolean createEmployee(Employee employee) throws SomethingWentWrong {
+		Connection connection = null;
+		
+		try {
+			connection  = ConnectToDataBase.makeConnnection();
+			PreparedStatement statement = connection.prepareStatement("insert into employee (ename, email_id, contact, dob, joining_date) values (?, ?, ?, ?, CURDATE())");
+			
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new SomethingWentWrong();
+		} finally {
+			try {
+				ConnectToDataBase.closeConnection(connection);
+			} catch (SQLException e) {
+				throw new SomethingWentWrong();
+			}
+		}
+		
+		return true;
+	}
 }
