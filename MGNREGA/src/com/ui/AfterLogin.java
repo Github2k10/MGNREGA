1bb60d6791d04b08bcbfcc31a06dde12a94dfd0e
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-02-26 21:43:20 +0530

    update

--- MGNREGA/src/com/ui/AfterLogin.java
+++ MGNREGA/src/com/ui/AfterLogin.java
@@ -226,15 +226,18 @@ public class AfterLogin {
 					}
 					break;
 				
-			case 7: System.out.print("\nEnter Gram Panchayat Member Id: ");try {
+			case 7: System.out.print("\nEnter Gram Panchayat Member Id: ");
+					try {
 						n = Integer.parseInt(scanner.nextLine());
 					} catch(Exception ex) {
 						System.out.println("Invalid Input!!!");
 						continue;
 					}
 					
-					System.out.print("Enter Gram Panchayat Member Id: ");try {
-						int p = Integer.parseInt(scanner.nextLine());
+					System.out.print("Enter Gram Panchayat Member Id: ");
+					int p = 0;
+					try {
+						p = Integer.parseInt(scanner.nextLine());
 					} catch(Exception ex) {
 						System.out.println("Invalid Input!!!");
 						continue;
@@ -251,7 +254,12 @@ public class AfterLogin {
 					break;
 				
 			case 8: System.out.print("\nEnter Project Id: ");
-					n = Integer.parseInt(scanner.nextLine());
+					try {
+						n = Integer.parseInt(scanner.nextLine());
+					} catch(Exception ex) {
+						System.out.println("Invalid Input!!!");
+						continue;
+					}
 					
 					try {
 						System.out.println("\n**************************************************************\n");
